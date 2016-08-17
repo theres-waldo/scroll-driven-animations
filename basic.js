d0deccbffb1640000cec0bdf15257238d8b2ec4a
Botond Ballo <botond@mozilla.com>
2016-08-17 17:01:19 -0400

    Update basic example to work with latest ScrollTimeline API

--- basic.js
+++ basic.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-  let scrollContainer = document.getElementById("scrollContainer");
+  let scrollable = document.getElementById("scrollable");
   let target = document.getElementById("target");
   target.style.animation = "rotateAnim 1000ms linear paused";
   
   let animation = target.getAnimations()[0];
   animation.ready.then(()=>{
-    let timeline = new ScrollTimeline(scrollContainer, "vertical");
+    let timeline = new ScrollTimeline({
+      scrollSource: scrollable,
+      orientation: "vertical"
+    });
     animation.timeline = timeline;
     animation.play();
   });
