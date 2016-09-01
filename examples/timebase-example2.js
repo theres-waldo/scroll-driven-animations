c3bd1dc127f3faa24246661652377687b90846eb
Botond Ballo <botond@mozilla.com>
2016-08-31 20:20:09 -0400

    Reformulate Example 2 to be entirely in terms of the JS API
    This avoids the animation being initially created with a document
    timeline.

--- examples/timebase-example2.js
+++ examples/timebase-example2.js
@@ -1,18 +1,21 @@
 document.addEventListener("DOMContentLoaded", ()=>{
   let scrollable = document.documentElement;
   let spinner = document.getElementById("spinner");
-
-  let animation = spinner.getAnimations()[0];
-  animation.ready.then(()=>{
-    let timeline = new ScrollTimeline({
-      scrollSource: scrollable, 
-      orientation: "vertical",
-      timeRange: 300,
-      startScrollOffset: "1000px",
-      endScrollOffset: "1600px"
-    });
-    animation.timeline = timeline;
-    animation.startTime = 0;
-    animation.play();
+  let effect = new KeyframeEffect(
+    spinner,
+    [{ transform: 'rotate(0.25turn)' },
+     { transform: 'rotate(0.75turn)' }],
+    { duration: 300,
+      fill: 'both',
+      iterations: Infinity,
+      easing: 'linear' });
+  let timeline = new ScrollTimeline({
+    scrollSource: scrollable, 
+    orientation: "vertical",
+    timeRange: 300,
+    startScrollOffset: "1000px",
+    endScrollOffset: "1600px",
   });
+  let animation = new Animation(effect, timeline);
+  animation.play();
 });
