a7eb68a3d3d4fa93de870ee24d66013c3412b9db
Botond Ballo <botond@mozilla.com>
2016-09-19 14:49:21 -0400

    Add a second example demonstrating a layout cycle

---
+++ examples/layout-cycle2.js
@@ -0,0 +1,32 @@
+window.addEventListener("scroll", ()=>{
+  let reporter = document.getElementById("offset");
+  reporter.textContent = "The scroll offset is " + document.documentElement.scrollTop;
+});
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  let scrollable = document.documentElement;
+  let top = document.getElementById("top");
+  let effect = new KeyframeEffect(
+    top,
+    [
+      { height: '1000px' },
+      { height: '2000px' },
+      { height: '1000px' }
+    ],
+    {
+      duration: 5000,
+      fill: 'both',
+      iterations: Infinity,
+      easing: 'linear'
+    }
+  );
+  let timeline = new ScrollTimeline({
+    scrollSource: scrollable,
+    orientation: "vertical",
+    timeRange: 5000,
+    startScrollOffset: "50%",
+    endScrollOffset: "100%"
+  });
+  let animation = new Animation(effect, timeline);
+  animation.play();
+});
