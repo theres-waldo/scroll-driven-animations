a2052223018b8623bc13eb0dc6b09860de688aaa
Botond Ballo <botond@mozilla.com>
2016-09-21 16:15:50 -0400

    Modify the first layout cycle example to make the cycle easier to see

--- examples/layout-cycle1.js
+++ examples/layout-cycle1.js
@@ -1,17 +1,28 @@
-window.addEventListener("scroll", ()=>{
-  let reporter = document.getElementById("offset");
-  reporter.textContent = "The scroll offset is " + document.documentElement.scrollTop;
-});
+function reportThings() {
+  let offset = document.getElementById("offset");
+  let scrollOffset = document.documentElement.scrollTop;
+  offset.textContent = "The scroll offset is " + scrollOffset;
+  
+  let time = document.getElementById("time");
+  let animation = document.getElementById("top").getAnimations()[0];
+  if (animation) {
+    let currentTime = animation.currentTime;
+    time.textContent = "The animation's current time is " + Math.round(currentTime);
+  } else {
+    time.textContent = "There is no animation";
+  }
+}
 
+window.addEventListener("scroll", reportThings);
 document.addEventListener("DOMContentLoaded", ()=>{
   let scrollable = document.documentElement;
   let top = document.getElementById("top");
   let effect = new KeyframeEffect(
     top,
     [
-      { height: '1000px' },
-      { height: '2000px' },
-      { height: '1000px' }
+      { height: '10000px' },
+      { height: '20000px' },
+      { height: '10000px' }
     ],
     {
       duration: 5000,
@@ -27,4 +38,5 @@ document.addEventListener("DOMContentLoaded", ()=>{
   });
   let animation = new Animation(effect, timeline);
   animation.play();
+  reportThings();
 });
